efe7965ae30d5aad466833f6476d6100a18bb861
Chris Chen <christopherchen@lbl.gov>
2020-10-02 13:46:36 -0700

    [andor_camera] handle andor_spec

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ try:
 except Exception as err:
     print('andor_camera library load error', err)
 from .andor_ccd import AndorCCDHW
-from .andor_ccd_readout import AndorCCDReadoutMeasure
+from .andor_ccd_readout import AndorCCDReadoutMeasure, AndorCCDStepAndGlue
 from .andor_ccd_kinetic_measure import AndorCCDKineticMeasure
 from .andor_spec_calib_measure import AndorSpecCalibMeasure
